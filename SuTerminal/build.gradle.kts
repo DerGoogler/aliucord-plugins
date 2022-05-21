70e816e8aab0046fd908708f5cfd4eb45a496eef
DerGoogler <support@dergoogler.com>
2022-05-21 18:05:42 +0200

    1.2.0

--- SuTerminal/build.gradle.kts
+++ SuTerminal/build.gradle.kts
@@ -1,9 +1,11 @@
-version = "1.1.0"
+version = "1.2.0"
 description = "Run superuser commands though Discord!"
 
 aliucord {
     changelog.set(
             """
+            # 1.2.0
+            * Add ability to send (default is false)
             # 1.1.0
             * Added support for custom mkshrc. (loads `/system/etc/mkshrc`)
             # 1.0.0
--- SuTerminal/src/main/java/com/dergoogler/plugins/SuTerminal.java
+++ SuTerminal/src/main/java/com/dergoogler/plugins/SuTerminal.java
@@ -24,39 +24,43 @@ public class SuTerminal extends Plugin {
     private final Logger log;
     private final SuperUser su;
     private final String DefaultExecute;
+    private String send;
 
     public SuTerminal() {
         this.log = new Logger("SuTerminal");
         this.su = new SuperUser();
         this.DefaultExecute = "echo \"Hello world!\"";
+        this.send = "false";
     }
 
     @Override
     public void start(Context context) {
         String keyDefaultEnv = "def env";
         String keyMkshrcEnv = "mkshrc env";
+        String keySend = "send";
         String customMkshrcEnv = "if [ -f /system/etc/mkshrc ]; then . /system/etc/mkshrc; fi; ";
-        boolean send = false;
         List<MessageEmbed> embeds = null;
         List<ApplicationCommandOption> options = Arrays.asList(
                 Utils.createCommandOption(ApplicationCommandType.STRING, keyDefaultEnv, "Creates an default environment"),
-                Utils.createCommandOption(ApplicationCommandType.STRING, keyMkshrcEnv, "Creates an custom mkshrc environment, e.g. View Googlers-Magisk-Repo/mkshrc to get Termux environment within Discord")
+                Utils.createCommandOption(ApplicationCommandType.STRING, keyMkshrcEnv, "Creates an custom mkshrc environment, e.g. View Googlers-Magisk-Repo/mkshrc to get Termux environment within Discord"),
+                Utils.createCommandOption(ApplicationCommandType.BOOLEAN, keySend, "Sends the output to given channel")
         );
 
         this.commands.registerCommand(
                 "su", "Run superuser command (requires root)",
                 options,
                 commandCxt -> {
+                    boolean sendKey = commandCxt.getBoolOrDefault(keySend, false);
                     if (commandCxt.containsArg(keyDefaultEnv)) {
                         String defaultEnv = commandCxt.getStringOrDefault(keyDefaultEnv, this.DefaultExecute);
-                        return new CommandsAPI.CommandResult(this.runSu(defaultEnv), embeds, send);
+                        return new CommandsAPI.CommandResult(this.runSu(defaultEnv), embeds, sendKey);
                     } else if (commandCxt.containsArg(keyMkshrcEnv)) {
                         String mkshrcEnv = commandCxt.getStringOrDefault(keyMkshrcEnv, this.DefaultExecute);
-                        return new CommandsAPI.CommandResult(this.runSu(customMkshrcEnv + mkshrcEnv), embeds, send);
+                        return new CommandsAPI.CommandResult(this.runSu(customMkshrcEnv + mkshrcEnv), embeds, sendKey);
                     } else {
                         log.warn("No command was executed");
                     }
-                    return new CommandsAPI.CommandResult(commandCxt.getRawContent(), embeds, send);
+                    return new CommandsAPI.CommandResult(commandCxt.getRawContent(), embeds, sendKey);
                 });
 
     }
